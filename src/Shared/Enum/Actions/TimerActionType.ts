be17eebc9d86c6b678e3309c6fdad01db4f67410
DESKTOP-P9EK4AV\Tobias Katra <tobias.katra@consid.se>
2024-02-13 13:59:21 +0100

    Removed unused state handlers

--- src/Shared/Enum/Actions/TimerActionType.ts
+++
@@ -1,7 +0,0 @@
-export enum TimerActionType {
-  StartTimer,
-  UpdateTimer,
-  SetTimerId,
-  StopTimer,
-  HideTimer
-}
--- src/Shared/Types/Actions/timerAction.ts
+++
@@ -1,8 +0,0 @@
-import { TimerActionType } from "../../Enum/Actions/TimerActionType";
-
-export type TimerAction = 
-  | {type: TimerActionType.StartTimer, payload: number}
-  | {type: TimerActionType.UpdateTimer, payload: number}
-  | {type: TimerActionType.SetTimerId, payload: NodeJS.Timer}
-  | {type: TimerActionType.StopTimer, payload: number}
-  | {type: TimerActionType.HideTimer};
